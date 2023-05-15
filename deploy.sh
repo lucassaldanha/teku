d219025bfe4ae85769e1db6d077e93419ce94e28
Simon Dudley <simon.dudley@consensys.net>
2023-05-15 13:00:23 +1000

    Add deployment script Signed-off-by: Simon Dudley <simon.dudley@consensys.net>

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+./gradlew installDist -x checkLicense
+REV=`git rev-parse --short HEAD`
+cp -R build/install/teku teku-beku-$REV
+tar cvf teku-beku-$REV.tar.gz teku-beku
+scp teku-beku-$REV.tar.gz 3.145.211.75:teku-beku-$REV.tar.gz
